6bc6f112cf2fd8e7615558988c63770d7a437dab
Brian aguiar <briantbini@outlook.com>
2021-05-03 15:58:33 -0300

    Comando de copia de arquivo e fazer ler o arquivo selecionado eexibir na tela

--- ArquivosC/Program.cs
+++ ArquivosC/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+using System.IO;
 namespace ArquivosC
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sourcePath = @"E:\C#\Projetos02\ArquivosC\file1.txt";
+            string targetPath = @"E:\C#\Projetos02\ArquivosC\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                // Faz a copia do arquivo file1
+                fileInfo.CopyTo(targetPath);
+
+                //Ler todas as linhas do arquivo e guardar no vetor 
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            } catch(IOException e)
+            {
+                Console.WriteLine("Erro ocorru !");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
